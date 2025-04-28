1357ce4559d70983275cf5655bcc0cfec846c317
Fedor <fedor.shopping@gmail.com>
2025-04-28 13:02:47 +0900

    update logic of prices microservice

--- src/pages/api/coin-prices.js
+++ src/pages/api/coin-prices.js
@@ -12,7 +12,7 @@ export const config = {
 
 let lastUpdated = false
 let cache = {}
-
+let updater = false
 
 async function reloadData() {
   const req = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=ETH,BNB,SOL', {
@@ -34,17 +34,15 @@ async function reloadData() {
       if (k === 'bnb') cache.bsc = cache[k] // duplicate
     }
   })
-
-  console.log(response.data, 'FETCHED')
+  const now = Date.now()
+  lastUpdated = now
 }
 
 const handler = async (req, res) => {
-  const now = Date.now()
-  if (!lastUpdated || !cache || now > lastUpdated + 5*60*1000) {
+  if (!updater) {
+    updater = setInterval(reloadData, 5*60*1000) // every 5 mins
+    // init data in cache
     await reloadData()
-    lastUpdated = now
-  } else {
-    console.log('Use cache')
   }
 
   res.status(200).json({ rates: cache, last_updated: lastUpdated })
