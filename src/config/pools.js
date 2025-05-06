a27a796d99853d568f42fda54dff4701d5a26aab
Fedor <fedor.shopping@gmail.com>
2025-05-07 07:37:51 +0900

    publish v2

--- src/config/pools.js
+++ src/config/pools.js
@@ -2,40 +2,40 @@
 const poolsConfig = {
   "eth": [
     {
-      "title": "ETH Light",
-      "address": "0xfd14BdD429f0Fe2D679f726A0E9D72cb0D0837B1",
+      "title": "ETH Light v2",
+      "address": "0xFAC3dD91C9530aF4Db6B104058Ea573239866AA6",
       "stake": 0.01,
       "stakeWei": "0x2386F26FC10000"
     },
     {
-      "title": "ETH Bronze",
-      "address": "0xD47774c6B5EB5012fA5D5AF88966B4c1043BBD2f",
+      "title": "ETH Bronze v2",
+      "address": "0xEFEDD851B15D9F02a2b99A7dede830be3e50E737",
       "stake": 0.05,
       "stakeWei": "0xB1A2BC2EC50000"
     },
     {
-      "title": "ETH Silver",
-      "address": "0x0A6d2CFE07C6a070F8a9aB365594F2fD0DcC9c7A",
+      "title": "ETH Silver v2",
+      "address": "0x7eF99FE708e2C910Dd8D297049Fc45e00c5d8854",
       "stake": 0.1,
       "stakeWei": "0x16345785D8A0000"
     }
   ],
   "bsc": [
     {
-      "title": "BSC Light",
-      "address": "0x460D4b774fe559F88827f375c0ae1C0C2fe77B0f", // "0x16dBA25057DB95920aa8E6167B4bEf04582B66C4",
+      "title": "BSC Light v2",
+      "address": "0x460D4b774fe559F88827f375c0ae1C0C2fe77B0f",
       "stake": 0.01,
       "stakeWei": "0x2386F26FC10000"
     },
     {
-      "title": "BSC Bronze",
-      "address": "0xd50b76e2c4205cCb89cB6461726C3033253b941f", // "0x143BBC939350340F7e8FCE544568dac9d043E6Da",
+      "title": "BSC Bronze v2",
+      "address": "0xd50b76e2c4205cCb89cB6461726C3033253b941f",
       "stake": 0.05,
       "stakeWei": "0xB1A2BC2EC50000"
     },
     {
-      "title": "BSC Silver",
-      "address": "0xCDeae25475e446DD467Ae31C04931D17013b60C4", // "0xB86D6e3ba6e1EF397A5c3d8796f18D56360CC505",
+      "title": "BSC Silver v2",
+      "address": "0xCDeae25475e446DD467Ae31C04931D17013b60C4",
       "stake": 0.1,
       "stakeWei": "0x16345785D8A0000"
     }
